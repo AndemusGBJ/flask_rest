b388974d5f2b11aa4722b5809291a03cf0165ff7
Albert Gubanja <andemus017@outlook.fr>
2021-10-24 16:41:27 +0200

    Api flaskresfull with msql

--- app.py
+++ app.py
@@ -5,19 +5,13 @@
 
 from flask import Flask, jsonify
 from flask_restful import Api, Resource, abort, reqparse
-from flaskext.mysql import MySQL
-
-main = Flask(__name__)
-api = Api(main)
+import requests
 
 
-main.config['MYSQL_DATABASE_USER'] = 'root'
-main.config['MYSQL_DATABASE_PASSWORD'] = ''
-main.config['MYSQL_DATABASE_DB'] = 'youri'
-main.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
-mysql = MySQL()
-mysql.init_app(main)
+app = Flask(__name__)
+api = Api(app)
+
 
 
 ETUDIANTS = {
@@ -73,57 +67,34 @@ class Etudiant(Resource):
 
         return ('Modifié avec succès', 200)
 
-"""
-Utilisation de MySql avec Flaskrestful.
-On crée un utilisateur avec un email et un password
-"""
-
-
-# MySQL configurations
-
-class CreateUser(Resource):
-
+class TextSimilarity(Resource):
     def post(self):
-        try:
-            # Parse the arguments
-            parser = reqparse.RequestParser()
-            parser.add_argument('id', type=int, help='ID to create user')
-            parser.add_argument('email', type=str, help='Email address to create user')
-            parser.add_argument('password', type=str, help='Password to create user')
-            args = parser.parse_args()
 
-            _userId = args['id']
-            _userEmail = args['email']
-            _userPassword = args['password']
+        url = "https://twinword-twinword-bundle-v1.p.rapidapi.com/text_similarity/"
 
-            conn = mysql.connect()
-            cursor = conn.cursor()
-            cursor.execute(''' INSERT INTO tblUser VALUES(%s,%s,%s)''', (_userId, _userEmail, _userPassword))
-            mysql.connect().commit()
-            cursor.callproc('spCreateUser', (_userId, _userEmail, _userPassword))
-            data = cursor.fetchall()
+        payload = "text1=The%20hippocampus%20is%20a%20major%20component%20of%20the%20brains%20of%20humans%20and%20other%20vertebrates.%20It%20belongs%20to%20the%20limbic%20system%20and%20plays%20important%20roles%20in%20the%20consolidation%20of%20information%20from%20short-term%20memory%20to%20long-term%20memory%20and%20spatial%20navigation.%20Humans%20and%20other%20mammals%20have%20two%20hippocampi%2C%20one%20in%20each%20side%20of%20the%20brain.%20The%20hippocampus%20is%20a%20part%20of%20the%20cerebral%20cortex%3B%20and%20in%20primates%20it%20is%20located%20in%20the%20medial%20temporal%20lobe%2C%20underneath%20the%20cortical%20surface.%20It%20contains%20two%20main%20interlocking%20parts%3A%20Ammon's%20horn%20and%20the%20dentate%20gyrus.&text2=An%20important%20part%20of%20the%20brains%20of%20humans%20and%20other%20vertebrates%20is%20the%20hippocampus.%20It's%20part%20of%20the%20limbic%20system%20and%20moves%20information%20from%20short-term%20to%20long-term%20memory.%20It%20also%20helps%20us%20move%20around.%20Humans%20and%20other%20mammals%20have%20two%20hippocampi%2C%20one%20on%20each%20side.%20The%20hippocampus%20is%20a%20part%20of%20the%20cerebral%20cortex%3B%20and%20in%20primates%20it%20is%20found%20in%20the%20medial%20temporal%20lobe%2C%20beneathe%20the%20cortical%20surface.%20It%20has%20two%20main%20interlocking%20parts%3A%20Ammon's%20horn%20and%20the%20dentate%20gyrus."
+        headers = {
+            'content-type': "application/x-www-form-urlencoded",
+            'x-rapidapi-key': "4dfeb488c9mshdeac92794c046a8p113fecjsn63f0f1bec84a",
+            'x-rapidapi-host': "twinword-twinword-bundle-v1.p.rapidapi.com"
+        }
 
-            return {'Email': args['email'], 'Password': args['password']}
+        response = requests.request("POST", url, data=payload, headers=headers)
 
-        except Exception as e:
-            return {'error': str(e)}
+        return (response.text)
 
-
-
-
-        # cursor = mysql.connection.cursor()
-        # cursor.execute(''' INSERT INTO info_table VALUES(%s,%s)''', (name, age))
-        # mysql.connection.commit()
-        # cursor.close()
-        # return f"Done!!"
 
 
 api.add_resource(listeEtudiants, '/etudiants')
 api.add_resource(Etudiant, '/etudiants/<string:matricule>')
-api.add_resource(CreateUser, '/CreateUser')
+api.add_resource(TextSimilarity, '/Text')
+
+
 
 
 
 if __name__ == '__main__':
-    main.run(debug=True, port=8000)
+    app.run(debug=True, port=8000)
+
+
 
